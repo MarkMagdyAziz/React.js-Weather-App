56c4517cf1b22954fb96b9d591959e75c17d1e88
Mark- Wahba <mark.aziz.magdy@gmail.com>
2020-06-03 12:45:58 +0200

    weather-app-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-function App() {
+
+const API_KEY = "309823633c5aeba9f126874921fce779";
+// Link 
+// http://api.openweathermap.org/data/2.5/weather?q=cairo,egypt&appid=e36ed364400282e43250b6c4c0274d44
+
+class App extends Component {
+  state = {
+    temperature: '',
+    city: '',
+    country: '',
+    humidity: '',
+    description: '',
+    error: ''
+  }
+  getWeather = async (e) => {
+    
+    e.preventDefault()
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+   
+    const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+    const data = await api.json();
+  
+    //console.log(data)
+
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error:'',
+      })
+    } else 
+    {
+      this.setState({
+        
+        temperature: '',
+        city: '',
+        country: '',
+        humidity: '',
+        description: '',
+        error:'Enter the country and city'
+      })
+        
+      }
+  }
+
+render () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="form-container">
+      <Form getWeather={this.getWeather}/>
+      <Weather
+        temperature= {this.state.temperature } 
+        city={this.state.city}
+        country={this.state.country}
+        humidity= {this.state.humidity}
+        description= {this.state.description}
+        error={this.state.error}
+      
+        />
+        </div>
     </div>
   );
+  }
 }
 
 export default App;
